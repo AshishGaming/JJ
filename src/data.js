897f8aad6ff62c58cb29a35bd6f1184031e6371b
AshishGaming <ashishautar2002@outlook.com>
2026-01-09 01:28:33 +0100

    Fix image casing for GitHub Pages (Linux)

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
-import pic1 from "./assets/PIC1.jpg";
-import pic2 from "./assets/PIC2.jpg";
-import pic3 from "./assets/PIC3.jpg";
-import pic4 from "./assets/PIC4.jpg";
-import pic5 from "./assets/PIC5.jpg";
+import pic1 from "./assets/pic1.jpg";
+import pic2 from "./assets/pic2.jpg";
+import pic3 from "./assets/pic3.jpg";
+import pic4 from "./assets/pic4.jpg";
+import pic5 from "./assets/pic5.jpg";
 
 export const photobookPages = [
   {
